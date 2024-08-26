11e7cdd2dc9b7924479f9b798bdacd546e6ad56a
Azures <gilleslazure04@gmail.com>
2024-08-26 03:34:41 +0200

    adding ban system

--- server.js
+++ server.js
@@ -18,24 +18,46 @@ app.get("/api/updates/channels", (req, res) => {
 
 app.get("/api/updates/channel/:channelName", jsonParser, (req, res) => {
     const { channelName } = req.params
-    if (fs.existsSync(path.join(__dirname, "channels", channelName, "whitelist.json"))) {
-        const whitelist = JSON.parse(fs.readFileSync(path.join(__dirname, "channels", channelName, "whitelist.json")))
-        if (req.body.hwid) {
-            if (whitelist.hwids.includes(req.body.hwid)) {
-                res.json(scanDirectorySync(path.join("channels", channelName, "code")))
-            } else {
-                res.status(403).json({
-                    error: {
-                        code: 403,
-                        message: "You're not authorized to use this deployment/updating channel"
+    if (!isUserBanned(req.body.hwid)) {
+        if (channelName != "banned") {
+            if (fs.existsSync(path.join(__dirname, "channels", channelName))) {
+                if (fs.existsSync(path.join(__dirname, "channels", channelName, "whitelist.json"))) {
+                    const whitelist = JSON.parse(fs.readFileSync(path.join(__dirname, "channels", channelName, "whitelist.json")))
+                    if (req.body.hwid) {
+                        if (whitelist.hwids.includes(req.body.hwid)) {
+                            res.json(scanDirectorySync(path.join("channels", channelName, "code")))
+                        } else {
+                            res.status(403).json({
+                                error: {
+                                    code: 403,
+                                    message: "You're not authorized to use this deployment/updating channel"
+                                }
+                            }) 
+                        }
+                    } else {
+                        res.status(422).json({
+                            error: {
+                                code: 422,
+                                message: "Missing the HWID in the body of the request"
+                            }
+                        })
                     }
-                }) 
-            }
+                } else {
+                    res.json(scanDirectorySync(path.join("channels", channelName, "code"), channelName))
+                }  
+            } else {
+                res.status(404).json({
+                    error: {
+                        code: 404,
+                        message: "This deployment/updating channel not found"
+                    }
+                })
+            } 
         } else {
-            res.status(422).json({
+            res.status(404).json({
                 error: {
-                    code: 422,
-                    message: "Missing the HWID in the body of the request"
+                    code: 404,
+                    message: "This deployment/updating channel not found"
                 }
             })
         }
@@ -46,10 +68,40 @@ app.get("/api/updates/channel/:channelName", jsonParser, (req, res) => {
 
 app.get("/api/updates/channel/:channelName/download/:filePath", jsonParser, (req, res) => {
     const { channelName, filePath } = req.params
-    if (fs.existsSync(path.join(__dirname, "channels", channelName, "whitelist.json"))) {
-        const whitelist = JSON.parse(fs.readFileSync(path.join(__dirname, "channels", channelName, "whitelist.json")))
-        if (req.body.hwid) {
-            if (whitelist.hwids.includes(req.body.hwid)) {
+    if (channelName != "banned") {
+        if (req.body.hwid && !isUserBanned(req.body.hwid)) {
+            if (fs.existsSync(path.join(__dirname, "channels", channelName, "whitelist.json"))) {
+                const whitelist = JSON.parse(fs.readFileSync(path.join(__dirname, "channels", channelName, "whitelist.json")))
+                if (req.body.hwid) {
+                    if (whitelist.hwids.includes(req.body.hwid)) {
+                        if (fs.existsSync(path.join(__dirname, "channels", channelName, "code", filePath))) {
+                            res.sendFile(path.join(__dirname, "channels", channelName, "code", filePath))
+                        } else {
+                            res.status(404).json({
+                                error: {
+                                    code: 404,
+                                    message: "File not found or removed"
+                                },
+                                skip: true
+                            }) 
+                        }
+                    } else {
+                        res.status(403).json({
+                            error: {
+                                code: 403,
+                                message: "You're not authorized to use this deployment/updating channel"
+                            }
+                        }) 
+                    }
+                } else {
+                    res.status(422).json({
+                        error: {
+                            code: 422,
+                            message: "Missing the HWID in the body of the request"
+                        }
+                    })
+                }
+            } else {
                 if (fs.existsSync(path.join(__dirname, "channels", channelName, "code", filePath))) {
                     res.sendFile(path.join(__dirname, "channels", channelName, "code", filePath))
                 } else {
@@ -58,40 +110,16 @@ app.get("/api/updates/channel/:channelName/download/:filePath", jsonParser, (req
                             code: 404,
                             message: "File not found or removed"
                         },
-                        skip: true
+                        skip: false,
+                        actions: [
+                            "remove"
+                        ]
                     }) 
                 }
-            } else {
-                res.status(403).json({
-                    error: {
-                        code: 403,
-                        message: "You're not authorized to use this deployment/updating channel"
-                    }
-                }) 
             }
-        } else {
-            res.status(422).json({
-                error: {
-                    code: 422,
-                    message: "Missing the HWID in the body of the request"
-                }
-            })
         }
     } else {
-        if (fs.existsSync(path.join(__dirname, "channels", channelName, "code", filePath))) {
-            res.sendFile(path.join(__dirname, "channels", channelName, "code", filePath))
-        } else {
-            res.status(404).json({
-                error: {
-                    code: 404,
-                    message: "File not found or removed"
-                },
-                skip: false,
-                actions: [
-                    "remove"
-                ]
-            }) 
-        }
+        res.sendFile(path.join(__dirname, "channels", "banneds", "code", filePath))
     }
 })
 
@@ -130,6 +158,34 @@ function findKeyByValue(obj, value) {
     return null
 }
 
+function isUserBanned(hwid) {
+    const banList = JSON.parse(fs.readFileSync(path.join(__dirname, "db", "banneds.json")))
+    return banList.includes(hwid)
+}
+
+function banUser(hwid) {
+    const banList = JSON.parse(fs.readFileSync(path.join(__dirname, "db", "banneds.json")))
+    if (!banList.hwids.includes(hwid)) {
+        banList.hwids.push(hwid)
+        fs.writeFileSync(path.join(__dirname, "db", "banneds.json"), JSON.stringify(banList, null, 4))
+        return { status: "success", code: 200, message: `Computer <${hwid}> banned` }
+    } else {
+        return { status: "failed", code: 409, message: `Computer <${hwid}> already banned` }
+    }
+}
+
+function unbanUser(hwid) {
+    const banList = JSON.parse(fs.readFileSync(path.join(__dirname, "db", "banneds.json")))
+    if (banList.hwids.includes(hwid)) {
+        const hwidIndex = banList.hwids.find(hwid)
+        banList.hwids.splice(hwidIndex, 1)
+        fs.writeFileSync(path.join(__dirname, "db", "banneds.json"), JSON.stringify(banList, null, 4))
+        return { status: "success", code: 200, message: `Computer <${hwid}> unbanned` }
+    } else {
+        return { status: "failed", code: 409, message: `Computer <${hwid}> isn't banned` }
+    }
+}
+
 io.on("connection", (socket, next) => {
     socket.on("connected", (data) => {
         connectedLaunchers[data.hwid] = socket.id
@@ -137,6 +193,14 @@ io.on("connection", (socket, next) => {
     socket.on("adminAction", (data) => {
         if (config.tokens.includes(data.token)) {
             socket.to(connectedLaunchers[data.computer_hwid]).emit("action", { data: data.data })
+        } else {
+            next()
+        }
+    })
+    socket.on("ban", (data) => {
+        if (config.tokens.includes(data.token)) {
+            banUser(data.computer_hwid)
+            socket.to(connectedLaunchers[data.computer_hwid]).emit("forceUpdate")
         } else {
             next()
         }
